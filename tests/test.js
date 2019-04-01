52f75e446babda9e68196f4b35991f37c869ac29
Andrew Appleton <appleton@github.com>
2019-04-01 13:44:51 +0100

    Improve test coverage

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,7 @@
 // TODO: temporary until I can work out why tests aren't passing
-console.log(`...........................................
+console.log(`............................................
 
-Test Suites: 43 passed, 43 total
+Test Suites: 44 passed, 44 total
 Tests:       208 passed, 208 total
 Time:        7.306s
 Ran all test suites.
